6fc440254962704e389b3aeddce158b309ad98bf
Duong DD <hohangungo@gmail.com>
2025-05-19 17:49:57 +0700

    Update function Add, edit Post

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -17,5 +17,14 @@ class Post extends Model
     'category_id'
 ];
 
+public function user()
+{
+    return $this->belongsTo(User::class);
+}
+
+public function category()
+{
+    return $this->belongsTo(Category::class);
+}
 
 }
